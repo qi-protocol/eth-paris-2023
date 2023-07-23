441e52c57e383daa60cc3fa30cbb163fcec5e1fa
FudgyDRS <yetimonsters@gmail.com>
2023-07-23 07:10:06 +0200

    finished plugin

--- contracts/contracts/plugin/GatTx/GatTx.sol
+++ contracts/contracts/plugin/GatTx/GatTx.sol
@@ -4,25 +4,16 @@ pragma solidity ^0.8.17;
 import "../BasePlugin.sol";
 import "./IGatTx.sol";
 import "../../safeLock/SafeLock.sol";
-import "../../libraries/AddressLinkedList.sol";
-import "../../libraries/SignatureDecoder.sol";
-import "@account-abstraction/contracts/core/Helpers.sol";
 import "../../interfaces/IExecutionManager.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../../interfaces/IPluginStorage.sol";
 
-contract GatTx is BasePlugin, IGatTx, SafeLock {
+abstract contract GatTx is BasePlugin, IGatTx, SafeLock {
     //using AddressLinkedList for mapping(address => address);
 
     uint256 nonce;
 
-    mapping(uint256 => bytes) private _calldata;
+    bytes32 constant ENABLE_GAT = keccak256(abi.encodePacked("ENABLE_GAT"));
 
-    constructor() SafeLock("PLUGIN_DAILYLIMIT_SAFELOCK_SLOT", 2 days) {}
-
-    struct Layout {
-        uint256 nonce;
-    }
+    constructor(bytes32 initData) SafeLock("PLUGIN_GATTX_SAFELOCK_SLOT", 2 days)  {}
 
     function _supportsHook() internal pure override returns (uint8 hookType) {
         hookType = PRE_HOOK;
@@ -44,25 +35,58 @@ contract GatTx is BasePlugin, IGatTx, SafeLock {
     // concept:
     // x bytes used for calldata to be executed, remaining bytes are ignored and can be used for storage data
     // we can there store GAT timestamp uint256 and nonce
-    function preHook(address target, uint256 value, bytes calldata data) external pure override {
+    function preHook(address target, uint256 value, bytes calldata data) external view override {
         (target, value, data);
-        bytes memory data_ = data;
-        uint256 size;
-        assembly { size := sub(calldatasize(), 52) }
-        
 
-        revert("GatTx: preHook not support");
+        // extract nonce and timestamp data from the 
+        uint256 size;
+        assembly { size := sub(calldatasize(), 56) }
+        bytes memory data_ = data[size:];
+
+        if (data_.length >= 64) {
+
+            // Parse the last 96 bytes into three variables
+            bytes32 key_;
+            //uint256 nonce_;
+            uint256 timestamp_;
+            assembly {
+                let dataEnd := add(data_, mload(data_))
+                timestamp_ := mload(sub(dataEnd, 32))
+                //nonce_ := mload(sub(dataEnd, 64))
+                key_ := mload(sub(dataEnd, 64))
+            }
+
+            // Check if the first one matches the constant keccak256("ENABLE_GAT")
+            // by adding the key check users can enable/disable this hook feature in their userop tx
+            if(key_ == ENABLE_GAT) {
+                require(block.timestamp < timestamp_, "GatTx: Execution too early");
+            }
+        }
     }
 
+    // check to extra and analyze data to check if failed userop failed caused by the this Hook or normal operations
+    function checkData(bytes calldata data) external pure override returns(uint256 timestamp_, bool enabled_) {
+        bytes memory data_ = data;
+        if (data_.length >= 64) {
+            bytes32 key_;
+            assembly {
+                let dataEnd := add(data_, mload(data_))
+                timestamp_ := mload(sub(data_, 32))
+                key_ := mload(sub(dataEnd, 64))
+            }
+
+            // Check key matches enabled state
+            enabled_ = key_ == ENABLE_GAT;
+        }
+    }
+
+    // postHook kees a history of our success
     function postHook(address target, uint256 value, bytes calldata data) external override {
         (target, value, data);
-        revert("GatTx: preHook not support");
+        nonce++;
+        emit GATSuccess(nonce, data);
+        //revert("GatTx: preHook not support");
     }
 
 
 }
-
-
-// each tx has a nonce 
-// each tx increments nonce
-// each tx has a Good After Timestamp value
--- contracts/contracts/plugin/GatTx/IGatTx.sol
+++ contracts/contracts/plugin/GatTx/IGatTx.sol
@@ -2,19 +2,9 @@
 pragma solidity ^0.8.17;
 
 interface IGatTx {
-    event DailyLimitChanged(address[] token, uint256[] limit);
+    event GATSuccess(uint256 nonce, bytes data);
     event PreSetDailyLimit(address[] token, uint256[] limit);
     event CancelSetDailyLimit(address[] token, uint256[] limit);
 
-    function reduceDailyLimits(address[] calldata token, uint256[] calldata amount) external;
-
-    function preSetDailyLimit(address[] calldata token, uint256[] calldata limit) external;
-
-    function cancelSetDailyLimit(address[] calldata token, uint256[] calldata limit) external;
-
-    function comfirmSetDailyLimit(address[] calldata token, uint256[] calldata limit) external;
-
-    function getDailyLimit(address wallet, address token) external view returns (uint256);
-
-    function getSpentToday(address wallet, address token) external view returns (uint256);
+    function checkData(bytes calldata data) external returns(uint256 timestamp_, bool enabled_);
 }
