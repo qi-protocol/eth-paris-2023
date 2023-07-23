48658dcdda1057abacf073cc99672ac62775e63e
FudgyDRS <yetimonsters@gmail.com>
2023-07-23 07:17:26 +0200

    cleanup

--- contracts/contracts/IntentFactory.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.17;
-
-// For this version created plugins can only be added to avalible hooks on deployment
-
--- contracts/doc/contract_architecture.md
+++
@@ -1,7 +0,0 @@
-
-
-# Contract architecture
-
-<p align="center">
-  <img src="./images//contract_architecture.png" />
-</p>
